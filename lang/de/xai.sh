5cb15d22bd54bead9acfdb88b764e2716c204060
flippo007 <max.gouilhers@gmail.com>
2018-04-17 20:33:23 +0200

    Translate :de:
    + xai.sh
    + utils_linux.sh

---
+++ lang/de/xai.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+flags='bc:ihjklmnp:qrs:uvwx:z'
+
+EOF
+}
+
+headline="NEU: Standard-Timeout aktualisieren unter Einstellungen > Audio"
+
+
+xai_get_current_dir () {
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do
+      DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+    done
+    echo "$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+}
+
+xai_dir="$(xai_get_current_dir)"
+
+
+cd "$xai_dir"
+
+shopt -s nocasematch
+source utils/utils.sh
+source utils/store.sh
+source utils/audio.sh
+source utils/configure.sh
+
+
+if [ "$EUID" -eq 0 ]; then
+    echo "FEHLER: Xaï kann nicht als root verwendet werden!" 1>&2
+    exit 1
+fi
